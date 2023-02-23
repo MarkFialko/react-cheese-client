ea13be4edb67223f954f6cbf8f2b6ff5e3c8fbb0
markfialko <markfialkofialko@gmail.com>
2023-02-23 16:55:54 +0500

    222

--- src/http/index.ts
+++ src/http/index.ts
@@ -19,6 +19,7 @@ $api.interceptors.response.use((config) => {
 }, async (error) => {
 
     const originalRequest = error.config
+    console.log(error)
     if (error.response.status == 401 && error.config && !error.config._isRetry) {
         originalRequest._isRetry = true
         try {
