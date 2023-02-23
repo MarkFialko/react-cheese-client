3ae24d20ac38f4e777deaf99fb2a69a61f2f8077
markfialko <markfialkofialko@gmail.com>
2023-02-23 17:49:01 +0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React, {useEffect} from 'react';
 import AppRouter from "./components/AppRouter";
 import {useAppDispatch} from "./hooks/useAppDispatch";
 import {checkAuth} from "./store/actions/authAction";
+import {useNavigate} from "react-router-dom";
 
 
 function App() {
 
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (localStorage.getItem("token")) {
             console.log("token here")
             dispatch(checkAuth())
         }
+        navigate('/')
     }, [])
 
     return (
